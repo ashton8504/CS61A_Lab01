d72b8b989283af230daf197e460a5aec2f5de1b7
Phaedrus <92445005+ashton8504@users.noreply.github.com>
2022-10-26 09:44:26 -0400

    Add files via upload

---
+++ lab01.py
@@ -0,0 +1,64 @@
+def falling(first_parameter, last_parameter):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    counter_start = 1
+    counter_stop = 0
+
+    while counter_stop <= last_parameter - 1:
+        counter_start = counter_start * (first_parameter - counter_stop)
+        counter_stop = counter_stop + 1
+
+    return counter_start
+
+
+print(falling(4, 3))
+
+
+def sum_digits(n):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    total = 0
+
+    while n > 0:
+        total = total + n % 10
+        n = n // 10
+
+    return total
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
